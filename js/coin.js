ca6fa1e1ea43e7e070346c833dc4f158876580b3
Josh <toyjtoy@gmail.com>
2015-11-01 14:19:17 -0500

    init commit

---
+++ js/coin.js
@@ -0,0 +1,48 @@
+$( document ).ready(function() {
+
+  $(function(){
+    var $img = $("#myimg");
+
+      function rotate(degree,speed,orientation) {
+      $img.css({ WebkitTransform: 'rotateY('+degree+'deg)'});
+      $img.css({ '-o-transform': 'rotateY('+degree+'deg)'});
+      $img.css({ '-transform': 'rotateY('+degree+'deg)'});
+      $img.css({ '-moz-transform': 'rotateY('+degree+'deg)'});
+
+      $img.css({ '-moz-transition': speed+'s'});
+      $img.css({ '-moz-transform-style': 'preserve-3d'});
+      $img.css({ '-webkit-transition': speed+'s'});
+      $img.css({ '-webkit-transform-style': 'preserve-3d'});
+      $img.css({ '-o-transition': speed+'s'});
+      $img.css({ '-o-transition-style': 'preserve-3d'});
+      $img.css({ '-transition': speed+'s'});
+      $img.css({ '-transform-style': 'preserve-3d'});
+      }
+      $('#flip').click(function(){
+
+          $speed = 0.05;
+          for($i=0;$i<=1440;$i++)
+          {
+              if($i % 100 == 0)
+              {
+                   $speed = $speed + 0.1 ;
+              }
+              rotate($i,$speed,'Y');
+          }
+          setTimeout(function(){
+              $('#myimg').removeAttr('style');
+          },3000);
+
+      });
+  });
+
+$( "#add1" ).click(function() {
+  $( "#h21" ).empty().append($("#opt1").val());
+
+});
+
+$( "#add2" ).click(function() {
+  $( "#h22" ).empty().append($("#opt2").val());
+});
+
+});
